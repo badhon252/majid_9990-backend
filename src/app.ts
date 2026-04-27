9e700923266e31df87cec691fb74078d6528bc1a
badhon252 <khalidhossainbadhon@gmail.com>
2026-04-27 07:27:14 +0600

    feat: add dashboard domain to CORS allowed origins

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-      origin: 'https://majid-website-two.vercel.app',
+      origin: 'https://majid-website-two.vercel.app, https://majid-dashboard.vercel.app',
 
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
       credentials: true,
